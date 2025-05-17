9c699efc328159aa44cb337537df6cb88052b8ce
1zumisawashun <shun.1zumisawa@gmail.com>
2025-05-17 20:53:37 +0900

    fix: コメントを追加

--- app/functions/libs/open-api/swagger.test.ts
+++ app/functions/libs/open-api/swagger.test.ts
@@ -8,6 +8,7 @@ beforeAll(() => {
   jestOpenAPI(absolutePath);
 });
 
+// TODO: jest-openapi自体は3年前から更新がないので別の方法でcontract testを実施できないか調査する
 describe("GET /api/hello", () => {
   it("should satisfy OpenAPI spec", async () => {
     const res = await axios.get("http://localhost:3000/api/hello");
